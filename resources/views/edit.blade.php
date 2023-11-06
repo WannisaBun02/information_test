d9b1ac79f45ed6691132ad6c49336dfddfee2e18
WannisaBun02 <147589600+WannisaBun02@users.noreply.github.com>
2023-11-06 11:22:38 +0700

    new commit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insert</title>
     <!-- css -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <!-- CDN -->
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
