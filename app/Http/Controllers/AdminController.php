76e3d33945cc6e86082f23171789be301f79e031
WannisaBun02 <147589600+WannisaBun02@users.noreply.github.com>
2023-11-06 09:27:27 +0700

    new commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,26 +7,30 @@ use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
-    function index(){
-        $user_names =DB::table('information_tests')->get();
-                return view('index',compact('user_names'));
+    function index()
+    {
+        $user_names = DB::table('information_tests')->get();
+        return view('index', compact('user_names'));
     }
-    function insert(){
+    function insert()
+    {
         return view('insert');
     }
-    function insertt(Request $request){
+    function insertt(Request $request)
+    {
         $request->validate([
-            'name'=>'required',
-            'age'=>'required',
+            'name' => 'required',
+            'age' => 'required',
             'gender' => 'required|not_in:เลือกเพศ',
-    ], [
-        'gender.not_in' => 'กรุณาเลือกเพศ',
-        'name.required'=> 'กรุณากรอกชื่อ',
-        'age.required'=> 'กรุณากรอกอายุ'
+        ], [
+            'gender.not_in' => 'กรุณาเลือกเพศ',
+            'name.required' => 'กรุณากรอกชื่อ',
+            'age.required' => 'กรุณากรอกอายุ'
         ]);
     }
-    function delete($id){
+    function delete($id)
+    {
         DB::table('information_tests')->where('id', $id)->delete();
-        return redirect('/index');
+        return redirect('/');
     }
 }
